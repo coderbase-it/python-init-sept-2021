589f4c9ff5a58492ba7450aece9459f6f59f581d
coderbase-it <48690897+coderbase-it@users.noreply.github.com>
2021-09-28 16:43:47 +0200

    voiture de course

--- cours/poo/voiture_de_course.py
+++ cours/poo/voiture_de_course.py
@@ -1,5 +1,21 @@
 from cours.poo.voiture import Voiture
 
 
-class VoitureDeCourse(Voiture):
-    pass
+class VoitureDeCourse(Voiture ):
+
+    def __init__(self, couleur, marque, speed):
+        Voiture.__init__(self, couleur, marque)
+        # super().__init__(couleur, marque)
+        self.speed = speed
+
+    def __str__(self):
+        return f'Je suis une voiture {self.brand} ' \
+               f'de couleur {self.color} ' \
+               f'vitesse: {self.speed}'
+
+    def parentPresenter(self):
+        return super().__str__()
+
+ma_ferrari = VoitureDeCourse('rouge', 'ferrari', 400)
+print(ma_ferrari)
+print(ma_ferrari.parentPresenter())

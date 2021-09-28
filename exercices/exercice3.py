6cea939ae8f71a684053321e2425bd9606698400
coderbase-it <48690897+coderbase-it@users.noreply.github.com>
2021-09-28 09:44:08 +0200

    exercice liste

---
+++ exercices/exercice3.py
@@ -0,0 +1,6 @@
+"""
+Écrivez un programme qui recherche le plus grand élément présent dans une liste donnée. Par exemple, si on l'appliquait à
+la liste [32, 5, 12, 8, 3, 75, 2, 15],
+ce programme devrait afficher : le plus grand élément de cette liste soit la valeur 75. (modifié)
+Bonus Exercice  : ne pas utiliser la function max , min et avg de Python ( ni la function sort ^^ )
+"""
---
+++ exercices/exercice4.py
@@ -0,0 +1,6 @@
+"""
+à partir de cette liste
+maliste = ['Jean', 'Maximilien',
+'Brigitte', 'Sonia', 'Jean-Pierre', 'Sandra']
+Fourni un programme permettant de séparer dans deux listes distinctes les prénoms avec  6 caractères ou plus et ceux avec moins de 6
+"""

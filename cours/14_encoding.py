cccda7c7bbdcf6e5e9baeb27d669c32dcd4b45d0
coderbase-it <48690897+coderbase-it@users.noreply.github.com>
2021-09-30 09:58:25 +0200

    Create 14_encoding.py

---
+++ cours/14_encoding.py
@@ -0,0 +1,23 @@
+# coding:utf-8
+#-*- coding: utf-8 -*-
+
+# she-bang / hash-bang
+
+#! python
+#!/usr/bin/env python
+#!/usr/bin/python
+
+
+
+import encodings
+
+# Deux syntaxe pour figer l'encoding dans le fichier
+
+print(encodings.aliases.aliases.items())
+
+name = 'engel' # str
+fistname = u'engel' # unicode
+
+# pip install unidecode
+# from unidecode import unidecode
+# print(unidecode(u"hélène")) # helene
